20afd25601a17dbdf4a76fd551e2a58c3bfca8d1
kunalMathuriya <kunalmathuria27@gmail.com>
2024-11-25 17:53:00 +0530

    Initial Commit

---
+++ start.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+module_name="api_support"
+main_module="api_service" #keep it empty "" if there is no main module 
+log_level="INFO" # INFO, DEBUG, ERROR
+
+########### DO NOT CHANGE ANY CODE OR TEXT AFTER THIS LINE #########
+
+
+. ~/.bash_profile
+
+build="${module_name}.jar"
+
+pid=`ps -ef | grep $build | grep java | grep $module_name | grep -v grep | awk '{print $2}'`
+
+if [ "${pid}" != "" ]  ## Process is currently running
+then
+  echo "${module_name} process is currently running with PID ${pid} ..."
+
+else  ## No process running
+
+  if [ "${main_module}" == "" ]
+  then
+     build_path="${APP_HOME}/${module_name}_module"
+     log_path="${LOG_HOME}/${module_name}_module"
+  else
+     if [ "${main_module}" == "utility" ] || [ "${main_module}" == "api_service" ] || [ "${main_module}" == "gui" ]
+     then
+       build_path="${APP_HOME}/${main_module}/${module_name}"
+       log_path="${LOG_HOME}/${main_module}/${module_name}"
+     else
+       build_path="${APP_HOME}/${main_module}_module/${module_name}"
+       log_path="${LOG_HOME}/${main_module}_module/${module_name}"
+     fi
+  fi
+
+  cd ${build_path}
+
+  ## Starting the process
+
+  echo "Starting ${module_name} module..."
+
+#  java -Xmx1024m -Xms256m -Dlog4j.configurationFile=./log4j2.xml -Dlog.level=${log_level} -Dlog.path=${log_path} -Dmodule.name=${module_name} -Dspring.config.location=file:${commonConfigurationFile},file:./application.properties -jar ${build} 1>/dev/null 2>${log_path}/${module_name}.error &
+
+
+  java -jar -Xmx1024m -Xms256m $build -Dspring.config.location=file:${commonConfigurationFile},:./application.properties 1>${log_path}/${module_name}.log 2>${log_path}/${module_name}.error &
+
+  ## check if process started successfully or not
+
+  pid=`ps -ef | grep $build | grep java | grep $module_name | grep -v grep | awk '{print $2}'`
+  if [ "$pid" == "" ]
+  then
+
+    echo "Failed to start $module_name process !!!"
+
+  else
+    
+    echo "$module_name process is started successfully with PID ${pid} ..."
+
+  fi
+
+fi
+
+#  java -jar -Xmx1024m -Xms256m $PNAME -Dspring.config.location=:./application.properties 1>/u02/eirsdata/logs/eirsbackend/log.log 2>/u02/eirsdata/logs/eirsbackend/error.log &
+
